03d373b705e4569e4f7cf79c0b362e395d358301
Jecheon Ryu <rjc1704@gmail.com>
2025-06-18 00:11:14 +0900

    수정

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Hello World! from Server-2</h1>");
 });
 
 app.get("/health", (req, res) => {
