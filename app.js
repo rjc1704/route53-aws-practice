f69b664d3728ad60d55c08ce74a866d48d710655
Jecheon Ryu <rjc1704@gmail.com>
2025-06-18 00:00:08 +0900

    수정

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/health", (req, res) => {
+  res.send("Health Check Success");
+});
+
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
